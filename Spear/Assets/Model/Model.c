c2bdfc5baee9e6418206e11fc0c3f0bb1b249f9f
Marc Sunet <jeannekamikaze@gmail.com>
2012-08-29 17:01:30 +0200

    Changed model_box to model_compute_boxes

--- Spear/Assets/Model/Model.c
+++ Spear/Assets/Model/Model.c
@@ -81,7 +81,7 @@ void model_copy_triangles (Model* model, unsigned frame, model_triangle* tris)
 }
 
 
-void model_box (Model* model, float* points)
+void model_compute_boxes (Model* model, float* points)
 {
     vec3* v = model->vertices;
         

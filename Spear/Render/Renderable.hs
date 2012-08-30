83876d723d598111741c83e570db74139cc0d22d
Marc Sunet <jeannekamikaze@gmail.com>
2012-08-30 19:23:18 +0200

    Removed Updatable and Renderable

--- Spear/Render/Renderable.hs
+++
@@ -1,8 +0,0 @@
-module Spear.Render.Renderable
-where
-
-
-class Renderable a where
-    
-    -- | Renders the given 'Renderable'.
-    render :: a -> IO ()
--- Spear/Updatable.hs
+++
@@ -1,9 +0,0 @@
-module Spear.Updatable
-where
-
-
--- | A type class for types that can update themselves given a time delta.
-class Updatable a where
-    
-    -- | Updates the given 'Updatable'.
-    update :: Float -> a -> a
